6cafa64a5523fc611bd64a98ca3a6f4a6f13122a
doganeraylin <doganeraylinn@gmail.com>
2023-04-04 14:58:39 +0200

    rendered home

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,35 +2,30 @@
   import React from 'react';
   import Head from 'next/head';
   import { destinationList } from '../graphql/queries'
-  import Button from '@mui/material/Button';
-
+  import Navbar from '../components/Navbar/Navbar';
+  import Home from '../components/Home/Home'
   function Destinations() {
-    <Head>
-      <meta name="viewport" content="initial-scale=1, width=device-width" />
-    </Head>
-    const { loading, error, data } = useQuery(destinationList
-    );
+    // <Head>
+    //   <meta name="viewport" content="initial-scale=1, width=device-width" />
+    // </Head>
+    // const { loading, error, data } = useQuery(destinationList
+    // );
 
-    if (loading) return <div>Loading...</div>;
-    if (error) return <div>Error: {error.message}</div>;
-    if (!data) return null;
+    // if (loading) return <div>Loading...</div>;
+    // if (error) return <div>Error: {error.message}</div>;
+    // if (!data) return null;
 
-    interface IDestination {
-      id: number;
-      attributes: {
-        name: string;
-        distance: number;
-      }
-    }
+    // interface IDestination {
+    //   id: number;
+    //   attributes: {
+    //     name: string;
+    //     distance: number;
+    //   }
+    // }
 
     return (
-      <ul>
-        <Button variant="contained">Hello World</Button>
-        {data.destinations.data.map((destination: IDestination) => (
-          <li key={destination.id}>{destination.attributes.name}</li>
-        ))}
-      </ul>
-    );
+     <>
+     <Home/>
+     </>    );
 }
-
-export default Destinations
+export default Destinations 
