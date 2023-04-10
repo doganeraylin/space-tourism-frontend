e30c290ea4643bb92d992d293ca0de8099f10641
doganeraylin <doganeraylinn@gmail.com>
2023-04-10 07:54:43 +0200

    added new queries

--- graphql/queries.js
+++ graphql/queries.js
@@ -1,17 +1,53 @@
 import { gql } from "@apollo/client"
 
 const destinationList = gql`
- query {
-  destinations {
-    data {
-      id
-      attributes {
-        name
-        distance
-      }
+    query {
+        destinations {
+        data {
+            id
+            attributes {
+            name
+            description
+            distance
+            travelTime
+            durationUnit
+            distanceUnit
+            image {
+                data {
+                id
+                attributes {
+                    url
+                }
+                }
+            }
+            }
+        }
+        }
     }
-  }
-}
 `
 
-export { destinationList }
+const crewList = gql`
+    query {
+        crews {
+            data {
+            id 
+            attributes {
+                role
+                fullName
+                bio
+                image {
+                data {
+                    id
+                    attributes {
+                        url
+                    }
+                }
+                }
+            }
+            }
+        }
+    }
+
+`
+
+export { destinationList, crewList }
