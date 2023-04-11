bdf80d2e13d8d488af25b22418e72ca624472d8a
doganeraylin <doganeraylinn@gmail.com>
2023-04-11 12:09:53 +0200

    added new containers and classnames

--- components/Destination/Destination.tsx
+++ components/Destination/Destination.tsx
@@ -52,43 +52,54 @@ const Destination = () => {
 
   return (
     <>
-    <div className={`${styles.container} d-flex flex-column align-items-center`}>
-      <h1 className={styles.title}><span>02</span>pick your destination</h1>
-      <div className={styles.imgContainer}>
-        {imageUrls.map((img, i) => 
-          <div key={i} className="d-flex justify-content-center">
+    <div className={`${styles.container} d-flex flex-column align-items-center flex-xl-row justify-content-xl-between`}>
+      <div className="d-xl-flex flex-xl-column justify-content-xl-center">
+          <div className={`${styles.imgContainer}`}>
+            <h1 className={`${styles.title} mb-xl-5`}><span>02</span>pick your destination</h1>
+            {imageUrls.map((img, i) => 
+            <div key={i} className="d-md-flex justify-content-md-center">
               {currentTab === `${i}` && 
-              <img className={`${styles.destinationImg} w-50`} src={img}></img>}
+              <img className={`${styles.destinationImg}`} src={img}></img>}
+            </div>
+            )}
           </div>
-        )}
-      </div >
-      <div className={`${styles.tabsContainer} d-flex justify-content-evenly w-50 mt-5 mb-5`}>
+      </div>
+      <div className={`${styles.destinationInfoWrapper} d-xl-flex flex-xl-column align-items-xl-start`}>
+        <div className={`${styles.tabsContainer} d-flex justify-content-evenly justify-content-xl-between w-50 mt-5 mb-5 mx-auto mx-xl-0`}>
         {tabs.map((tab, i) =>
             <button 
-            key={i} 
-            id={tab.id} 
-            disabled={currentTab === `${tab.id}`} 
-            onClick={(handleTabClick)}
-            className={styles.tabBtn}>
-              {tab.tab}
+              key={i} 
+              id={tab.id} 
+              disabled={currentTab === `${tab.id}`} 
+              onClick={(handleTabClick)}
+              className={styles.tabBtn}>
+                {tab.tab}
             </button>
         )}
+        </div>
+        <div>
+          {destinationData.map((dest, i) =>
+            <div key={i}  >
+                {currentTab === `${i}` && 
+                <div className={`${styles.textContainer} d-flex flex-column align-items-center align-items-xl-start`}>
+                  <p className={styles.name}>{dest.attributes.name}</p>
+                  <p className={`${styles.description} text-md-center text-xl-start`}>{dest.attributes.description}</p>
+                  <div className="d-md-flex justify-content-md-around w-100 justify-content-xl-start">
+                    <div className="text-center text-xl-start me-xl-5">
+                      <p className={styles.distanceTitle}>avg.distance</p>
+                      <p className={styles.distance}>{dest.attributes.distance} {dest.attributes.distanceUnit}</p>
+                    </div>
+                    <div className="text-center text-xl-start ms-xl-5">
+                      <p className={styles.travelTimeTitle}>est. travel time</p>
+                      <p className={styles.travelTime}>{dest.attributes.travelTime} {dest.attributes.durationUnit}</p>
+                    </div>
+                  </div>
+                </div>}
+            </div>
+            )}
+        </div>
       </div>
-      <div>
-        {destinationData.map((dest, i) =>
-          <div key={i}  >
-              {currentTab === `${i}` && 
-              <div className="d-flex flex-column align-items-center">
-                <p className={styles.name}>{dest.attributes.name}</p>
-                <p className={styles.description}>{dest.attributes.description}</p>
-                <p className={styles.distanceTitle}>avg.distance</p>
-                <p className={styles.distance}>{dest.attributes.distance} {dest.attributes.distanceUnit}</p>
-                <p className={styles.travelTimeTitle}>est. travel time</p>
-                <p className={styles.travelTime}>{dest.attributes.travelTime} {dest.attributes.durationUnit}</p>
-              </div>}
-          </div>
-          )}
-      </div>
+      
     </div>
     </>
   )
