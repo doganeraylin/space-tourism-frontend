daf83117307cf87a3a7f90c339d6ab27bf03cda0
doganeraylin <doganeraylinn@gmail.com>
2023-04-11 08:08:08 +0200

    added new classnames

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -3,8 +3,7 @@ import Navbar from '../Navbar/Navbar'
 
 const Home = () => {
     return (
-        <div className={`${styles.container} container text-center pt-3`}>
-            <Navbar/>
+        <div className={`${styles.container} container pt-3 d-flex flex-column align-items-center d-xl-flex flex-xl-row justify-content-lg-between align-items-lg-center`}>
             <div className={styles.textContainer}>
                 <h1 className={styles.title}> you want to travel to <span>space</span> </h1>
                 <p className={styles.description}>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
