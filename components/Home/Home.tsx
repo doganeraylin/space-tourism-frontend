ebfb84fc216e8e96de54028bd8e162705e509918
doganeraylin <doganeraylinn@gmail.com>
2023-04-17 10:22:44 +0200

    added animation to explore btn

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,18 +1,65 @@
+import React, { useRef, useLayoutEffect, useEffect, useState } from "react";
+import { useRouter } from 'next/router'
+import { gsap} from "gsap";
+import { useIsomorphicLayoutEffect } from '../../helpers/isomorphicEffect';
 import styles from './Home.module.css'
-import Navbar from '../Navbar/Navbar'
+
 
 const Home = () => {
+    const root = useRef()
+    const router = useRouter()
+    const [btnEffect, setBtnEffect] = useState(false)
+    let mm = gsap.matchMedia();
+
+useEffect(() => {
+    if (btnEffect) {
+        mm.add({
+            isMobile: "(max-width: 767px)",
+            isDesktop: "(min-width: 768px)"
+        },(context) => {
+            let { isMobile, isDesktop } = context.conditions
+            let tl = gsap.timeline();
+            tl.to(root.current, {
+            rotation: "+=360",
+            scale: isMobile ? 22 : 12,
+            ease: "circ.out",
+            duration: 2,
+            onComplete: () => {
+            console.log("Animation complete!");
+            router.push("/destination"); 
+            }
+        });
+            return () => {
+                tl.kill(); 
+            };
+        })
+    }
+}, [btnEffect]);
+ 
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+        
+        setBtnEffect(true)
+        e.preventDefault()
+    }
+
     return (
-        <div className={`${styles.container} container pt-3 d-flex flex-column align-items-center d-xl-flex flex-xl-row justify-content-lg-between align-items-lg-center`}>
+        <div  className={`${styles.container}  container pt-3 d-flex flex-column align-items-center d-xl-flex flex-xl-row justify-content-lg-between align-items-lg-center`}>
             <div className={styles.textContainer}>
                 <h1 className={styles.title}> you want to travel to <span>space</span> </h1>
                 <p className={styles.description}>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
             </div>
-            <div>
-                <button className={styles.exploreBtn} type="button">explore</button>
+            <div ref={root}>
+                <button 
+                    ref={root}
+                    className={styles.exploreBtn} 
+                    type="button"
+                    onClick={handleClick}>
+                    explore
+                </button>
             </div>
         </div>
     )
 }
+
 
 export default Home 
