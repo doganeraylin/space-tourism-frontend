4dcb07ef877e8833e8b96ef4de8cd02cfc620313
doganeraylin <doganeraylinn@gmail.com>
2023-04-19 14:58:51 +0200

    edited config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,34 @@
+// module.exports = {
+//   webpack: (config) => {
+//     config.module.rules.push({
+//       test: /\.(graphql|gql)$/,
+//       exclude: /node_modules/,
+//       loader: 'graphql-tag/loader',
+//     });
+//     return config;
+//   },
+// };
+
 module.exports = {
-  webpack: (config) => {
+  webpack: (config, options) => {
     config.module.rules.push({
       test: /\.(graphql|gql)$/,
       exclude: /node_modules/,
       loader: 'graphql-tag/loader',
     });
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next',
+            name: 'static/images/[name].[hash].[ext]',
+          },
+        },
+      ],
+    });
+
     return config;
   },
 };
