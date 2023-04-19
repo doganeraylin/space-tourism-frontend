04b8d734c9f86f858da566ce1dea5a8cbe4a1bd5
doganeraylin <doganeraylinn@gmail.com>
2023-04-19 10:50:14 +0200

    edited the animation

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,40 +1,52 @@
-import React, { useRef, useLayoutEffect, useEffect, useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { useRouter } from 'next/router'
-import { gsap} from "gsap";
-import { useIsomorphicLayoutEffect } from '../../helpers/isomorphicEffect';
+import { gsap } from "gsap";
 import styles from './Home.module.css'
 
 
 const Home = () => {
-    const root = useRef()
+
+    const exploreBtnExpand = useRef()
+    const exploreBtnRotate = useRef()
     const router = useRouter()
     const [btnEffect, setBtnEffect] = useState(false)
     let mm = gsap.matchMedia();
 
-useEffect(() => {
-    if (btnEffect) {
-        mm.add({
-            isMobile: "(max-width: 767px)",
-            isDesktop: "(min-width: 768px)"
-        },(context) => {
-            let { isMobile, isDesktop } = context.conditions
-            let tl = gsap.timeline();
-            tl.to(root.current, {
-            rotation: "+=360",
-            scale: isMobile ? 22 : 12,
-            ease: "circ.out",
-            duration: 2,
-            onComplete: () => {
-            console.log("Animation complete!");
-            router.push("/destination"); 
-            }
-        });
-            return () => {
-                tl.kill(); 
-            };
-        })
+    useEffect(() => {
+        if (btnEffect) {
+            mm.add({
+                isMobile: "(max-width: 767px)",
+                isDesktop: "(min-width: 768px)"
+            },(context) => {
+                let { isMobile, isDesktop } = context.conditions
+                let tl = gsap.timeline();
+                tl.to(exploreBtnExpand.current, {
+                rotation: "+=360",
+                scale: isMobile ? 22 : 12,
+                ease: "circ.out",
+                duration: 2,
+                onComplete: () => {
+                console.log("Animation complete!");
+                router.push("/destination"); 
+                },
+            
+            });
+                return () => {
+                    tl.kill(); 
+                };
+            })
+        }
+    }, [btnEffect]);
+
+
+    useEffect(() => {
+    let ctx = gsap.context(() => {
+        gsap.to(exploreBtnRotate.current, { rotation: "+=460", duration: 3, y: -100, repeat: -1, yoyo: true});
+    }, exploreBtnRotate);
+    return () => {
+        ctx.kill()
     }
-}, [btnEffect]);
+    }, []);
  
     const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
         
@@ -48,9 +60,9 @@ useEffect(() => {
                 <h1 className={styles.title}> you want to travel to <span>space</span> </h1>
                 <p className={styles.description}>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
             </div>
-            <div ref={root}>
+            <div ref={exploreBtnExpand}>
                 <button 
-                    ref={root}
+                   ref={exploreBtnRotate}
                     className={styles.exploreBtn} 
                     type="button"
                     onClick={handleClick}>
