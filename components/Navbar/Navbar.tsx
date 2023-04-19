d10e9c64fa8ed5064b264f67a8c377729c7ffd96
doganeraylin <doganeraylinn@gmail.com>
2023-04-19 14:45:35 +0200

    added animation

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,16 +1,40 @@
 import Link from "next/link"
-import { useRouter } from "next/router";
+import { useEffect } from "react"
+import { useRouter } from "next/router"
+import { gsap } from "gsap"
 import styles from './Navbar.module.css'
 
 const Navbar = () => {
+    const router = useRouter()
     const links = [
         { href: '/', text: 'home', pageNumber: "00" },
         { href: '/destination', text: 'destination', pageNumber: "01" },
         { href: '/crew', text: 'crew', pageNumber: "02" },
         { href: '/technology', text: 'technology', pageNumber: "03" }
-    ];
+    ]
 
-    const router = useRouter();
+    useEffect(() => {
+        const tl = gsap.timeline()
+        const activeLink = document.querySelector(`.${styles.active} a`)
+        
+        tl.to(activeLink, {
+            duration: 1,
+            opacity: 0.5,
+            y: 200,
+            ease: 'power2.out',
+      
+        })
+        tl.to(activeLink, {
+            duration: 1,
+            opacity: 1,
+            ease: 'power2.out',
+        
+        })
+        return () => {
+            tl.kill()
+        }
+  }, [])
+
     const handleClick = (e: React.MouseEvent<HTMLImageElement>) => {
         e.preventDefault()
         router.push("/")
@@ -28,12 +52,12 @@ const Navbar = () => {
             <button className={`${styles.navbarToggler} navbar-toggler`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className={`${styles.navbarTogglerIcon} navbar-toggler-icon`}></span>
             </button>
-            <div className="collapse navbar-collapse d-md-flex justify-content-md-end align-items-md-start" id="navbarSupportedContent">
-                <ul className={`${styles.linksContainer} navbar-nav my-2 my-md-0`}>
+            <div  className="collapse navbar-collapse d-md-flex justify-content-md-end align-items-md-start" id="navbarSupportedContent">
+                <ul  className={`${styles.linksContainer} navbar-nav my-2 my-md-0`}>
                     {links.map((link, index) => (
-                        <li key={index} className={`${styles.links} ${router.pathname === link.href ? styles.active : ''}`}>
-                            <Link href={link.href}>
-                                <a><span className="fw-bold me-2 d-md-none d-xl-inline">{link.pageNumber}</span>{link.text}</a>
+                        <li  key={index}  className={`${styles.links} ${router.pathname === link.href ? styles.active : ''}`}>
+                            <Link  href={link.href}>
+                                <a ><span className="fw-bold me-2 d-md-none d-xl-inline">{link.pageNumber}</span>{link.text}</a>
                             </Link>
                         </li>
                     ))}
