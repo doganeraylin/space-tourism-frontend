7b34a59ad4d9b362a4fb4e2617f7ae226ec2b526
doganeraylin <doganeraylinn@gmail.com>
2023-04-25 09:42:24 +0200

    loading component

---
+++ components/Loading/Loading.tsx
@@ -0,0 +1,7 @@
+import styles from './Loading.module.css'
+
+const Loading = () => 
+    <div className={styles.loading}>Loading
+    <span className="span"></span>
+    </div>
+export default Loading
