ff47ddd8abfd3ba499fe8c55c0ab78718455ef46
doganeraylin <doganeraylinn@gmail.com>
2023-04-25 11:34:53 +0200

    reordered

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
-
-import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css"
+import "../styles/globals.css";
 import type { AppProps } from 'next/app'
 import { ApolloProvider } from '@apollo/client';
 import useApollo from '../hooks/useApollo';
